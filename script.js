25d88ed3e3c879ad01a4cb667ccca01d99ccd027
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-05-21 22:00:01 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function ficaAzul() {
+    const botao = document.getElementById("botao");
+    botao.style.backgroundColor = "blue";
+}
+
+const botao = document.getElementById("botao");
+botao.addEventListener("click", ficaAzul);
