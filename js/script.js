839479e568d6dde19e0dc301e4b8b3d4bf03b50d
baps-reidelas2014 <gustavo-baps@educar.rs.gov.br>
2023-08-28 19:47:20 -0300

    commit 28/08/2023

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ var ativadoP = false;
 document.addEventListener("DOMContentLoaded", function(){
   var corBotaoP = document.getElementById("botaoProtanopia");
   var corBotaoD = document.getElementById("botaoDeuteranopia");
+  var corBotaoT = document.getElementById("botaoTritanopia");
+  
 
   corBotaoP.addEventListener("click", function(){
     ativadoP = !ativadoP;
     corBotaoD.setAttribute('style', "background-color: #D9D9D9");
     corBotaoP.setAttribute('style', 'background-color: #656565');
+    corBotaoT.setAttribute('style', "background-color: #D9D9D9");
     if(ativadoP){
       chrome.tabs.query({}, function(tabs){
         tabs.forEach(function(tab){
@@ -36,6 +39,7 @@ document.addEventListener("DOMContentLoaded", function(){
     ativadoD = !ativadoD;
     corBotaoP.setAttribute('style', "background-color: #D9D9D9");
     corBotaoD.setAttribute('style', 'background-color: #656565');
+    corBotaoT.setAttribute('style', "background-color: #D9D9D9")
     if(ativadoD){
       chrome.tabs.query({}, function(tabs){
         tabs.forEach(function(tab){
@@ -58,20 +62,71 @@ document.addEventListener("DOMContentLoaded", function(){
       });
     }
   });
+  var ativadoT = false;
+  corBotaoT.addEventListener("click", function(){
+    ativadoT = !ativadoT;
+    corBotaoD.setAttribute('style', "background-color: #D9D9D9");
+    corBotaoP.setAttribute('style', 'background-color: #D9D9D9');
+    corBotaoT.setAttribute('style', 'background-color: #656565');
+    if(ativadoT){
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        tabs.forEach(function(tab){
+          chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            function: mudaCorTritanopia,
+          });
+        });
+      });
+    } 
+    else{
+      corBotaoT.setAttribute('style', 'background-color: #D9D9D9');
+    }
+  });
+  
 });
 
+function mudaCorTritanopia() {
+  var elementos = document.querySelectorAll("*");
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].addEventListener("click", capturarClique, { once: true });
+  }
+
+  async function capturarClique(event) {
+    var alvo = event.target;
+    var style = getComputedStyle(alvo);
+    var bgColor = style.backgroundColor;
+    var rgbMatch = bgColor.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+
+    if (rgbMatch) {
+      var colorRGB = rgbMatch.slice(1).map(value => parseInt(value, 10));
+
+      getColorName(colorRGB).then(colorName => {
+        console.log("cor em RGB: ", colorRGB.join(", "));
+        console.log("Nome da cor: ", colorName);
+      });
+    } else {
+      console.error("nao foi possível extrair a cor");
+    }
+  }
+  async function getColorName(rgb) {
+    var stringRgb = rgb.join(",");
+    var apiUrl = `https://www.thecolorapi.com/id?rgb=${stringRgb}`;
+    try {
+      var resp = await fetch(apiUrl);
+      var data = await resp.json();
+      if (resp.ok) {
+        return data.name.value || "Unknown";
+      } else {
+        console.error('erro na resposta da api: ', data);
+        return "Unknown";
+      }
+    } catch (error) {
+      console.error('erro: ', error);
+      return 'unknown';
+    }
+  }
+}
 function mudaCorProtanopia(){
-  var imagens = document.querySelectorAll("img");
-  var videos = document.querySelectorAll("video");
-
-  for(var i = 0; i < imagens.length; i++){
-    imagens[i].style.filter = "brightness(1.2) contrast(1.2)";
-  }
-
-  for(var j = 0; j < videos.length; j++){
-    videos[j].style.filter = "brightness(1.2) contrast(1.2)";
-  }
-
   var elementos = document.querySelectorAll("*");
   for(var i = 0; i < elementos.length; i++){
     var estilo = getComputedStyle(elementos[i]);
@@ -103,8 +158,14 @@ function mudaCorProtanopia(){
 
 function mudaCorDeuteranopia(){
   var elementos = document.querySelectorAll("*");
-  for(var i = 0; i < elementos.length; i++){
-    elementos[i].style.filter = "hue-rotate(2.3deg)";
+  for(var i = 0; i<elementos.length; i++){
+    var estilo = getComputedStyle(elementos[i]);
+    var cor = estilo.color;
+    var corBG = estilo.getPropertyValue("background-color");
+    
+    if(corBG.startsWith("rgb")){
+      elementos[i].style.filter = "hue-rotate(2deg)";
+    }
   }
 }
 
